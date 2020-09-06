fade11b0178ae9430d040ac353b33ed2e951d069
Kun Lin <kun.lin.596@gmail.com>
2020-09-06 17:03:06 +0900

    Add LeNet implementation on tf

---
+++ lenet_tf.py
@@ -0,0 +1,142 @@
+import tensorflow as tf
+from tensorflow.contrib.layers import flatten
+import numpy as np
+
+def get_output_size(input, step):
+    pass
+
+def get_mnist():
+    from tensorflow.examples.tutorials.mnist import input_data
+
+    mnist = input_data.read_data_sets("MNIST_data/", reshape=False)
+    return mnist
+
+def read_data(mnist, datatype='train'):
+    X, y = getattr(mnist, datatype).images, getattr(mnist, datatype).labels
+    # X_validation, y_validation = mnist.validation.images, mnist.validation.labels
+    # X_test, y_test             = mnist.test.images, mnist.test.labels
+
+    assert(len(X) == len(y))
+
+    print("Image Shape: {}".format(X[0].shape))
+    print("Set:         {} samples".format(len(X)))
+
+    return X, y
+
+
+def normalize_data(X):
+    # Pad images with 0s
+    X = np.pad(X, ((0, 0), (2, 2),(2, 2), (0, 0)), 'constant')
+        
+    print("Updated Image Shape: {}".format(X[0].shape))
+    return X
+
+
+EPOCHS = 10
+BATCH_SIZE = 128
+
+def get_LeNet(X):
+    mu = 0
+    sigma = 0.1
+
+    # TODO: Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x6.
+    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 1, 6), mean=mu, stddev=sigma))
+    conv1_b = tf.Variable(tf.zeros(6))
+    conv1 = tf.nn.conv2d(X, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
+
+    # TODO: Activation.
+    conv1 = tf.nn.relu(conv1)
+
+    # TODO: Pooling. Input = 28x28x6. Output = 14x14x6.
+    conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+    # TODO: Layer 2: Convolutional. Output = 10x10x16.
+    conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean=mu, stddev=sigma))
+    conv2_b = tf.Variable(tf.zeros(16))
+    conv2 = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
+    
+    # TODO: Activation.
+    conv2 = tf.nn.relu(conv2)
+
+    # TODO: Pooling. Input = 10x10x16. Output = 5x5x16.
+    conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+    # TODO: Flatten. Input = 5x5x16. Output = 400.
+    fc0 = flatten(conv2)
+    
+    # TODO: Layer 3: Fully Connected. Input = 400. Output = 120.
+    fc1_W = tf.Variable(tf.truncated_normal([400, 120], mean=mu, stddev=sigma))
+    fc1_b = tf.Variable(tf.zeros(120))
+    fc1 = tf.matmul(fc0, fc1_W) + fc1_b
+    
+    # TODO: Activation.
+    fc1 = tf.nn.relu(fc1)
+
+    # TODO: Layer 4: Fully Connected. Input = 120. Output = 84.
+    fc2_W = tf.Variable(tf.truncated_normal([120, 84], mean=mu, stddev=sigma))
+    fc2_b = tf.Variable(tf.zeros(84))
+    fc2 = tf.matmul(fc1, fc2_W) + fc2_b
+    
+    # TODO: Activation.
+    fc2 = tf.nn.relu(fc2)
+
+    # TODO: Layer 5: Fully Connected. Input = 84. Output = 10.
+    fc3_W = tf.Variable(tf.truncated_normal([84, 10], mean=mu, stddev=sigma))
+    fc3_b = tf.Variable(tf.zeros(10))
+    logits = tf.matmul(fc2, fc3_W) + fc3_b
+
+    return logits
+if __name__ == "__main__":
+    # Input definitions
+    x = tf.placeholder(tf.float32, (None, 32, 32, 1))
+    y = tf.placeholder(tf.int32, (None))
+    one_hot_y = tf.one_hot(y, 10)
+
+    # LeNet definitions
+    rate = 0.001
+    logits = get_LeNet(x)
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=logits)
+    loss_operation = tf.reduce_mean(cross_entropy)
+    optimizer = tf.train.AdamOptimizer(learning_rate=rate)
+    training_operation = optimizer.minimize(loss_operation)
+
+    correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+    accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    saver = tf.train.Saver()
+
+    # Data
+    mnist = get_mnist()
+    X_train, y_train = read_data(mnist, 'train')
+    X_train = normalize_data(X_train)
+    X_validation, y_validation = read_data(mnist, 'validation')
+    X_validation = normalize_data(X_validation)
+
+    def evaluate(X_data, y_data):
+        num_examples = len(X_data)
+        total_accuracy = 0
+        sess = tf.get_default_session()
+        for offset in range(0, num_examples, BATCH_SIZE):
+            batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+            accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+            total_accuracy += (accuracy * len(batch_x))
+        return total_accuracy / num_examples
+
+    from sklearn.utils import shuffle
+    with tf.Session() as session:
+        session.run(tf.global_variables_initializer())
+        num_examples = len(X_train)
+        for i in range(EPOCHS):
+            X_train, y_train = shuffle(X_train, y_train)
+            for offset in range(0, num_examples, BATCH_SIZE):
+                end = offset + BATCH_SIZE
+                batch_X = X_train[offset: end]
+                batch_y = y_train[offset: end]
+                session.run(training_operation, feed_dict={x: batch_X, y: batch_y})
+            
+            validation_accuracy = evaluate(X_validation, y_validation)
+            print("EPOCH {} ...".format(i+1))
+            print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+            print()
+        
+        saver.save(session, './lenet')
+        print("Model saved")
